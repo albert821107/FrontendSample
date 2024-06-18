7e03170497adea25697c0bfd0a173772d3836864
bm5apa <timelessheaven@gmail.com>
2024-06-18 21:21:52 +0800

    frontend done

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,44 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [data1, setData1] = useState('');
+  const [data2, setData2] = useState('');
+
+   const handleSubmit = (event) => {
+        event.preventDefault();
+
+        fetch('http://localhost:3000/api/data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ data1: data1, data2: data2 }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    };
 
   return (
     <>
       <h1>Sign Up For a Account!</h1>
 
       <div className="card">
-        <form>
+        <form onSubmit={handleSubmit}>
         <div className="input-account">
-           <label for="account">Account :</label>
-           <input name="account" id="account" type="text"></input>
+           <label>Account :
+           <input name="account" id="account" type="text" value={data1} onChange={(e) => setData1(e.target.value)}></input></label>
         </div>
         <div className="input-password">
-           <label for="password">Password :</label>
-          <input name="password" id="password" type="password"></input>
+           <label>Password :
+          <input name="password" id="password" type="password" value={data2} onChange={(e) => setData2(e.target.value)}></input></label>
           </div>
-          <div className="register">
-            <input className="register-button" type="submit" value="Register" />
+          <div className="register-section">
+            <button className="register-button" type="submit">Register</button>
           </div>
           </form>
       </div>
